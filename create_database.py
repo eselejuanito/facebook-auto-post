65feb0f664af2a3dbc406cea564fc6235edbe808
eselejuanito <eselejuanito@gmail.com>
2024-08-20 06:29:15 -0700

    Update create_database.py, change the name of the table

--- create_database.py
+++ create_database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 con = sqlite3.connect('articles.db')
 cursorObj = con.cursor()
-cursorObj.execute("CREATE TABLE IF NOT EXISTS post(id integer PRIMARY KEY, title text NOT NULL, price integer NOT NULL check (price > 0), category text NOT NULL, state text NOT NULL, type text, option text, description text NOT NULL, path text NOT NULL, groups text, label text)")
+cursorObj.execute("CREATE TABLE IF NOT EXISTS item(id integer PRIMARY KEY, title text NOT NULL, price integer NOT NULL check (price > 0), category text NOT NULL, state text NOT NULL, type text, option text, description text NOT NULL, path text NOT NULL, groups text, label text)")
 cursorObj.execute("CREATE TABLE IF NOT EXISTS settings(user_email_login TEXT PRIMARY KEY,user_password_login TEXT NOT NULL,'language' TEXT NOT NULL,images_path TEXT NOT NULL,time_to_sleep REAL NOT NULL CHECK (time_to_sleep > 0),browser TEXT NOT NULL)")
 con.commit()
 con.close()
